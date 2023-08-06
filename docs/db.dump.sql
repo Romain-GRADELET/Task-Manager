174a9c6c6c2c84aae8e1f3c9aa530f1ac5254268
Romain Gradelet <romain.gradelet73@outlook.fr>
2023-08-06 21:54:29 +0200

    update sql dump

--- docs/db.dump.sql
+++ docs/db.dump.sql
@@ -1,29 +1,69 @@
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Adminer 4.7.6 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'L''identifiant de la catégorie',
+  `name` varchar(50) NOT NULL COMMENT 'Le nom de la catégorie',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1,	'home'),
+(4,	'work'),
+(6,	'food');
+
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE `tags` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'L''identifiant du tag',
+  `label` varchar(50) NOT NULL COMMENT 'nom du tag',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp() COMMENT 'La date de création du tag',
+  `updated_at` timestamp NULL DEFAULT NULL COMMENT 'La date de la dernière mise à jour du tag',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `tags` (`id`, `label`, `created_at`, `updated_at`) VALUES
+(1,	'urgent',	'2023-03-29 14:30:35',	NULL),
+(2,	'perso',	'2023-04-04 13:07:53',	NULL),
+(4,	'idea',	'2023-04-04 13:07:45',	NULL);
+
+DROP TABLE IF EXISTS `tag_task`;
+CREATE TABLE `tag_task` (
+  `task_id` bigint(20) unsigned NOT NULL,
+  `tag_id` int(10) unsigned NOT NULL,
+  KEY `task_id` (`task_id`),
+  KEY `tag_id` (`tag_id`),
+  CONSTRAINT `tag_task_ibfk_3` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tag_task_ibfk_4` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 DROP TABLE IF EXISTS `tasks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tasks` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` tinyint(4) NOT NULL DEFAULT 0,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `category_id` int(10) unsigned DEFAULT NULL,
+  `tag_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tag_id` (`tag_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`),
+  CONSTRAINT `tasks_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+INSERT INTO `tasks` (`id`, `title`, `status`, `created_at`, `updated_at`, `category_id`, `tag_id`) VALUES
+(169,	'faire le dossier de projet',	0,	'2023-08-02 10:55:52',	'2023-08-02 10:55:52',	4,	NULL),
+(170,	'faire les courses',	0,	'2023-08-02 10:56:06',	'2023-08-02 10:56:06',	6,	NULL),
+(172,	'faire le ménage',	0,	'2023-08-02 10:56:35',	'2023-08-02 10:56:35',	1,	NULL),
+(174,	'Continuer le dossier professionnelle',	0,	'2023-08-02 10:59:57',	'2023-08-02 10:59:57',	4,	NULL);
 
-INSERT INTO `tasks` (`id`, `title`, `created_at`, `updated_at`) VALUES
-(1, 'Acheter une salade', '2022-08-21 19:13:59', '2022-08-21 19:13:59'),
-(2, 'Manger la salade', '2022-08-21 19:14:18', '2022-08-21 19:14:18'),
-(3, 'Laver son assiette', '2022-08-21 19:14:33', '2022-08-21 19:14:33');
+-- 2023-08-06 19:48:20
